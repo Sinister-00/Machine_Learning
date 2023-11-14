5cafc75c28f1d35a8fc2d3d5f5dfd39dd3430e09
Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>
2023-11-14 09:50:49 +0530

    Added comments
    Signed-off-by: Swapnil Dhamu <swapnildhamu@Swapnils-MacBook-Pro.local>

--- KMEANS/include/kmeans.hpp
+++ KMEANS/include/kmeans.hpp
@@ -9,28 +9,28 @@
 #include <map>
 #include "data_handler.hpp"
 
-typedef struct cluster
+typedef struct cluster // cluster struct
 {
-    std::vector<double> *centroid;
-    std::vector<data *> *cluster_points;
-    std::map<int, int> class_counts;
-    int most_freq_class;
-    cluster(data *d)
+    std::vector<double> *centroid;       // centroid of the cluster
+    std::vector<data *> *cluster_points; // data points in the cluster
+    std::map<int, int> class_counts;     // class counts in the cluster (class, count)
+    int most_freq_class;                 // most frequent class in the cluster
+    cluster(data *d)                     // constructor for the cluster struct that takes a data point
     {
-        centroid = new std::vector<double>;
-        cluster_points = new std::vector<data *>;
-        for (auto v : *(d->get_feature_vector()))
+        centroid = new std::vector<double>;       // initialize the centroid vector
+        cluster_points = new std::vector<data *>; // initialize the cluster points vector
+        for (auto v : *(d->get_feature_vector())) // for each value in the data point's feature vector
         {
             centroid->push_back(v); // initialize the centroid to the first data point
         }
-        cluster_points->push_back(d);
+        cluster_points->push_back(d);     // push back the data point to the cluster points vector
         class_counts[d->get_label()] = 1; // initialize the class counts to the first data point
         most_freq_class = d->get_label();
     }
-    void add_to_cluster(data *val)
+    void add_to_cluster(data *val) // add a data point to the cluster
     {
-        int prevSize = cluster_points->size();
-        cluster_points->push_back(val);
+        int prevSize = cluster_points->size(); // get the previous size of the cluster
+        cluster_points->push_back(val);        // push back the data point to the cluster points vector
         for (int i = 0; i < centroid->size() - 1; i++)
         {
             double v = centroid->at(i);
@@ -39,7 +39,7 @@ typedef struct cluster
             v /= (double)cluster_points->size();
             centroid->at(i) = v;
         }
-        if (class_counts.find(val->get_label()) == class_counts.end())
+        if (class_counts.find(val->get_label()) == class_counts.end()) // if the class is not in the class counts
         {
             class_counts[val->get_label()] = 1;
         }
@@ -47,24 +47,24 @@ typedef struct cluster
         {
             class_counts[val->get_label()]++;
         }
-        set_most_freq_class();
+        set_most_freq_class(); // set the most frequent class in the cluster
     }
-    void set_most_freq_class()
+    void set_most_freq_class() // set the most frequent class in the cluster
     {
-        int best_class = -1;
-        int freq = 0;
-        for (auto it : class_counts)
+        int best_class = -1;         // -1 means no class is the best class
+        int freq = 0;                // frequency of the best class
+        for (auto it : class_counts) // for each class count
         {
-            if (it.second > freq)
+            if (it.second > freq) // if the frequency is greater than the current frequency
             {
                 freq = it.second;
                 best_class = it.first;
             }
         }
-        most_freq_class = best_class;
+        most_freq_class = best_class; // set the most frequent class to the best class
     }
 
-} cluster_t;
+} cluster_t; // cluster type
 
 class kmeans : public common_data
 {
@@ -74,10 +74,12 @@ class kmeans : public common_data
 
 public:
     kmeans(int);
+    // ~kmeans();
     void initialize_clusters();
     void initialize_clusters_for_each_class();
     void train();
     double euclidean_distance(std::vector<double> *, data *);
+    double manhattan_distance(std::vector<double> *, data *);
     double validate_performance();
     double test_performance();
 };
--- KMEANS/src/kmeans.cpp
+++ KMEANS/src/kmeans.cpp
@@ -10,87 +10,99 @@ void kmeans::initialize_clusters()
 {
     for (int i = 0; i < number_of_clusters; i++)
     {
-        int idx = rand() % (train_data->size());
+        int idx = rand() % (train_data->size()); // rand value between 0 and train_data->size()
         while (used_indices->find(idx) != used_indices->end())
         {
-            idx = rand() % (train_data->size());
+            idx = rand() % (train_data->size()); // if the index is already used, get a new one
         }
-        clusters->push_back(new cluster_t(train_data->at(idx)));
-        used_indices->insert(idx);
+        clusters->push_back(new cluster_t(train_data->at(idx))); // in clusters, add a new cluster with the centroid being the data point at idx
+        used_indices->insert(idx);                               // add the index to the used indices
     }
 }
 void kmeans::initialize_clusters_for_each_class()
 {
-    std::unordered_set<int> classes_used;
-    for (int i = 0; i < train_data->size(); i++)
+    std::unordered_set<int> classes_used;        // to keep track of the classes used
+    for (int i = 0; i < train_data->size(); i++) // for each data point
     {
-        if (classes_used.find(train_data->at(i)->get_label()) == classes_used.end())
+        if (classes_used.find(train_data->at(i)->get_label()) == classes_used.end()) // if the class is not used
         {
-            clusters->push_back(new cluster_t(train_data->at(i)));
-            classes_used.insert(train_data->at(i)->get_label());
-            used_indices->insert(i);
+            clusters->push_back(new cluster_t(train_data->at(i))); // push back a new cluster with the centroid being the data point at i
+            classes_used.insert(train_data->at(i)->get_label());   // add the class to the used classes (for the train data)
+            used_indices->insert(i);                               // add the index to the used indices (for the train data)
         }
     }
 }
 void kmeans::train()
 {
-    while (used_indices->size() != train_data->size())
+    while (used_indices->size() != train_data->size()) // while there are still data points not used
     {
-        int idx = (rand() % (train_data->size()));
-        // int idx = 0; // to speed up the process  
-        while (used_indices->find(idx) != used_indices->end())
+        int idx = (rand() % (train_data->size())); // get a random index between 0 and train_data->size()
+        // int idx = 0; // to speed up the process
+        while (used_indices->find(idx) != used_indices->end()) // if the index is already used, get a new one
         {
             idx = (rand() % (train_data->size()));
             // idx++;
         }
-        double min_dist = std::numeric_limits<double>::max();
-        int best_cluster = -1;
-        for (int i = 0; i < clusters->size(); i++)
+        double min_dist = std::numeric_limits<double>::max(); // initialize the minimum distance to the maximum double value
+        int best_cluster = -1;                                // -1 means no cluster is the best cluster
+        for (int i = 0; i < clusters->size(); i++)            // for each cluster
         {
-            double curr_dist = euclidean_distance(clusters->at(i)->centroid, train_data->at(idx));
-            if (curr_dist < min_dist)
+            // double curr_dist = manhattan_distance(clusters->at(i)->centroid, train_data->at(idx)); // get the distance between the centroid and the data point at idx
+            double curr_dist = euclidean_distance(clusters->at(i)->centroid, train_data->at(idx)); // get the distance between the centroid and the data point at idx
+            if (curr_dist < min_dist)                                                              // if the distance is less than the minimum distance
             {
-                min_dist = curr_dist;
-                best_cluster = i;
+                min_dist = curr_dist; // update the minimum distance
+                best_cluster = i;     // update the best cluster
             }
         }
-        clusters->at(best_cluster)->add_to_cluster(train_data->at(idx));
-        used_indices->insert(idx);
+        clusters->at(best_cluster)->add_to_cluster(train_data->at(idx)); // add the data point at idx to the best cluster
+        used_indices->insert(idx);                                       // add the index to the used indices
     }
 }
 double kmeans::euclidean_distance(std::vector<double> *centeroid, data *d)
 {
-    double dist = 0.0;
-    for (int i = 0; i < centeroid->size(); i++)
+    double dist = 0.0;                          // initialize the distance to 0
+    for (int i = 0; i < centeroid->size(); i++) // for each value in the centroid
     {
-        dist += pow(centeroid->at(i) - d->get_feature_vector()->at(i), 2);
+        dist += pow(centeroid->at(i) - d->get_feature_vector()->at(i), 2); // add the squared difference between the centroid and the data point
     }
-    return sqrt(dist);
+    return sqrt(dist); // return the square root of the distance
+}
+
+double kmeans::manhattan_distance(std::vector<double> *centeroid, data *d)
+{
+    double dist = 0.0;                          // initialize the distance to 0
+    for (int i = 0; i < centeroid->size(); i++) // for each value in the centroid
+    {
+        dist += abs(centeroid->at(i) - d->get_feature_vector()->at(i)); // add the absolute difference between the centroid and the data point
+    }
+    return dist; // return the distance
 }
 double kmeans::validate_performance()
 {
-    double correct = 0.0;
-    for (auto d : *valid_data)
+    double correct = 0.0;      // initialize the number of correct predictions to 0
+    for (auto d : *valid_data) // for each data point in the validation data
     {
-        double min_dist = std::numeric_limits<double>::max();
-        int best_cluster = -1;
-        for (int i = 0; i < clusters->size(); i++)
+        double min_dist = std::numeric_limits<double>::max(); // initialize the minimum distance to the maximum double value
+        int best_cluster = -1;                                // -1 means no cluster is the best cluster
+        for (int i = 0; i < clusters->size(); i++)            // for each cluster in the clusters vector
         {
-            double curr_dist = euclidean_distance(clusters->at(i)->centroid, d);
-            if (curr_dist < min_dist)
+            // double curr_dist = manhattan_distance(clusters->at(i)->centroid, d); // get the distance between the centroid and the data point
+            double curr_dist = euclidean_distance(clusters->at(i)->centroid, d); // get the distance between the centroid and the data point
+            if (curr_dist < min_dist)                                            // if the distance is less than the minimum distance
             {
-                min_dist = curr_dist;
-                best_cluster = i;
+                min_dist = curr_dist; // update the minimum distance
+                best_cluster = i;     // update the best cluster
             }
         }
-        if (clusters->at(best_cluster)->most_freq_class == d->get_label())
+        if (clusters->at(best_cluster)->most_freq_class == d->get_label()) // if the most frequent class in the best cluster is the same as the data point's class
         {
-            correct++;
+            correct++; // increment the number of correct predictions
         }
     }
-    return 100.0 * (correct / (double)valid_data->size());
+    return 100.0 * (correct / (double)valid_data->size()); // return the percentage of correct predictions
 }
-double kmeans::test_performance()
+double kmeans::test_performance() // same as validate_performance() but for the test data
 {
     double correct = 0.0;
     for (auto d : *test_data)
@@ -99,6 +111,7 @@ double kmeans::test_performance()
         int best_cluster = -1;
         for (int i = 0; i < clusters->size(); i++)
         {
+            // double curr_dist = manhattan_distance(clusters->at(i)->centroid, d);
             double curr_dist = euclidean_distance(clusters->at(i)->centroid, d);
             if (curr_dist < min_dist)
             {
@@ -113,25 +126,34 @@ double kmeans::test_performance()
     }
     return 100.0 * (correct / (double)test_data->size());
 }
+// kmeans::~kmeans()
+// {
+//     // for (auto c : *clusters) // for each cluster in the clusters vector
+//     // {
+//     //     delete c; // delete the cluster
+//     // }
+//     // delete clusters;     // delete the clusters vector
+//     // delete used_indices; // delete the used indices
+// }
 
-int main()
+int main() // driver code
 {
-    data_handler *dh = new data_handler();
-    dh->read_feature_vector("../Dataset/train-images-idx3-ubyte");
-    dh->read_label_vector("../Dataset/train-labels-idx1-ubyte");
-    dh->split_data();
-    dh->count_classes();
-    double performance = 0, best_performance = 0.0;
-    int best_k = 1;
-    for (int k = dh->get_num_classes(); k < dh->get_train_data()->size(); k++)
+    data_handler *dh = new data_handler();                                     // initialize the data handler
+    dh->read_feature_vector("../Dataset/train-images-idx3-ubyte");             // read the feature vector from the train images file
+    dh->read_label_vector("../Dataset/train-labels-idx1-ubyte");               // read the label vector from the train labels file
+    dh->split_data();                                                          // split the data into train, validation, and test data
+    dh->count_classes();                                                       // count the number of classes
+    double performance = 0, best_performance = 0.0;                            // initialize the performance variables
+    int best_k = 1;                                                            // assuming that the best k is 1
+    for (int k = dh->get_num_classes(); k < dh->get_train_data()->size(); k++) // starts from the number of classes and goes up to the number of data points
     {
-        kmeans *km = new kmeans(k);
-        km->set_test_data(dh->get_test_data());
-        km->set_valid_data(dh->get_validation_data());
-        km->set_train_data(dh->get_train_data());
-        km->initialize_clusters_for_each_class();
-        km->train();
-        performance = km->validate_performance();
+        kmeans *km = new kmeans(k);                    // initialize the kmeans object
+        km->set_test_data(dh->get_test_data());        // set the test data
+        km->set_valid_data(dh->get_validation_data()); // set the validation data
+        km->set_train_data(dh->get_train_data());      // set the train data
+        km->initialize_clusters_for_each_class();      // initialize the clusters for each class
+        km->train();                                   // train the model
+        performance = km->validate_performance();      // get the performance on the validation data
         std::cout << "Current performance: " << performance << "%"
                   << " at k = " << k << "\n";
         if (performance > best_performance)
